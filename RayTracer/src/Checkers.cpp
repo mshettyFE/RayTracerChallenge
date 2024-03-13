597425e276ca6dccc8701415a6ac573bb7dea96d
Mihir Shetty <ms6556@columbia.edu>
2024-03-12 21:47:56 -0400

    Changed floor to std::round

--- RayTracer/src/Checkers.cpp
+++ RayTracer/src/Checkers.cpp
@@ -3,7 +3,7 @@
 #include "Shape.h"
 #include "Pattern.h"
 #include "Checkers.h"
-#include <math.h>
+#include <cmath>
 #include <stdexcept>
 
 
@@ -17,9 +17,9 @@ Color Checkers::at(const Tuple& pt) const {
     if(pt.type() != TupType::POINT){
         throw std::invalid_argument("pt must be a point in Checkers");
     }
-    int x = static_cast<int>(floor(pt[0]));
-    int y = static_cast<int>(floor(pt[1]+glob_resolution));
-    int z = static_cast<int>(floor(pt[2]));
+    int x = static_cast<int>(std::round(pt[0]));
+    int y = static_cast<int>(std::round(pt[1]));
+    int z = static_cast<int>(std::round(pt[2]));
     int checker = x+y+z;
 //    int checker = x+z;
     if(static_cast<int>(checker)%2  == 0){
--- RayTracer/src/Stripes.cpp
+++ RayTracer/src/Stripes.cpp
@@ -1,7 +1,7 @@
 #include "Stripes.h"
 #include "Constants.h"
 #include "Shape.h"
-#include <math.h>
+#include <cmath>
 #include <stdexcept>
 
 Stripes::Stripes(const Color& a, const Color& b, Matrix trans) : Pattern(trans,"Stripes"){
@@ -13,7 +13,7 @@ Color Stripes::at(const Tuple& pt) const{
     if(pt.type() != TupType::POINT){
         throw std::invalid_argument("pt must be a point in Stripes");
     }
-    if((static_cast<int>(floor(pt[0]))%2 ) == 0){
+    if((static_cast<int>(std::round(pt[0]))%2 ) == 0){
         return color_a;
     }
     return color_b;
