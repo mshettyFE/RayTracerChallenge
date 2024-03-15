057820c9d05951566895fa05f8a2682b75dc090a
Mihir Shetty <ms6556@columbia.edu>
2024-03-14 20:31:22 -0400

    Added OBJParser test

---
+++ RayTracer/tests/OBJParser_test.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "OBJParser.h"
+
+TEST(OBJParser, Gibberish){
+    Parser p;
+    auto out = p.read("There was a young lady named Bright\n"
+"who traveled much faster than light.\n"
+"She set out one day\n"
+"in a relative way,\n"
+"and came back the previous night.\n", false);
+    EXPECT_EQ(p.get_invalid_reads(),5);
+}
+
+TEST(OBJParser, VertexParser){
+    Parser p;
+    auto out = p.read("v -1 1 0\n"
+"v -1.0000 0.5000 0.0000\n"
+"v 1 0 0\n"
+"v 1 1 0\n", false);
+    EXPECT_EQ(p.get_invalid_reads(),0);
+    EXPECT_THROW({
+        try{
+            p.get_vertex(0);
+        }
+        catch(const std::invalid_argument &e){
+            EXPECT_STREQ( "Out of bounds on parser (0 is out of bounds)", e.what() );
+            throw;
+        }
+    }
+    ,std::invalid_argument);
+    EXPECT_EQ(p.get_vertex(1),GenPoint(-1,1,0));
+    EXPECT_EQ(p.get_vertex(2),GenPoint(-1,0.5,0));
+    EXPECT_EQ(p.get_vertex(3),GenPoint(1,0,0));
+    EXPECT_EQ(p.get_vertex(4),GenPoint(1,1,0));
+}
